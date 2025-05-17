d8915b432ba78567f3e6e576da817f214591e804
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-05-17 08:14:18 +0100

    api updated to include fastapi binary

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 
 sys.path.append(".")
 
+from core.config import FASTAPI_BIN
+
 
 if __name__ == "__main__":
-    os.system("fastapi run ./api/app.py --port=9500 --host=0.0.0.0")
+    os.system(f"{FASTAPI_BIN} run ./api/app.py --port=9500 --host=0.0.0.0")
